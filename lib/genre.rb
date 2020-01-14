305f339e16a709a0867a34111393bc5ec3ea544e
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-01-14 20:48:18 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,23 @@
 class Genre
 
-
+  attr_accessor :name
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def save
+    @@all << self
+  end
+  
 
 end
